88581b89cd58c74605fd48a1dcfa94c18514e631
Paulo Henrique Estevam <paulo@kluh.com.br>
2019-12-04 06:31:24 -0300

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,81 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, ViewChild} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
+import {distinctUntilChanged, filter, map, startWith, takeUntil} from 'rxjs/operators';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {MatSidenav} from '@angular/material';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'hcpdemo-ui';
+export class AppComponent implements OnDestroy {
+  breadcrumbs$: Observable<Array<BreadCrumb>>;
+  smallScreen: boolean;
+  @ViewChild(MatSidenav, {static: false}) sidenav: MatSidenav;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.breadcrumbs$ = this.router.events.pipe(
+      takeUntil(this.destroy$),
+      startWith(new NavigationEnd(0, '', '')),
+      filter((event) => {
+        return event instanceof NavigationEnd;
+      }),
+      distinctUntilChanged(),
+      map((event) => {
+        return buildBreadCrumb(this.activatedRoute.root);
+      })
+    );
+  }
+
+  protected destroy$ = new Subject();
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+
+
+  getSidenavMode(): 'over' | 'push' | 'side' {
+    if (!this.smallScreen) {
+      return 'side';
+    } else {
+      return 'over';
+    }
+  }
+
+  getSidenavOpened(): boolean {
+    return !this.smallScreen;
+  }
+}
+
+export interface BreadCrumb {
+  label: string;
+  url: string;
+}
+
+export function buildBreadCrumb(route: ActivatedRoute, url: string = '',
+                                breadcrumbs: Array<BreadCrumb> = []): Array<BreadCrumb> {
+  // If no routeConfig is avalailable we are on the root path
+  const label = route.routeConfig && route.routeConfig.data ? route.routeConfig.data.breadcrumb : 'Home';
+  const path = route.routeConfig ? route.routeConfig.path : '';
+  // In the routeConfig the complete path is not available,
+  // so we rebuild it each time
+  const nextUrl = `${url}${path}/`;
+  const breadcrumb = {
+    label,
+    url: nextUrl
+  };
+  // const newBreadcrumbs = [...breadcrumbs, breadcrumb];
+  const newBreadcrumbs = [breadcrumb];
+  if (route.firstChild && route.children && route.children.length > 0) {
+    // If we are not on our current path yet,
+    // there will be more children to look after, to build our breadcumb
+    return buildBreadCrumb(route.firstChild, nextUrl, newBreadcrumbs);
+  }
+  return newBreadcrumbs;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: '', component: HomeComponent, data: {
+      breadcrumb: 'Home'
+    }
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
